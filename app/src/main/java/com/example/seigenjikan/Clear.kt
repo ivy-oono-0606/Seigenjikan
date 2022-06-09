bd18c28102c28df799604c09ff7e4399feea6970
izumi731 <o.izumi_731@docomo.ne.jp>
2022-06-09 15:20:29 +0900

    ota Clear&GameOver

---
+++ app/src/main/java/com/example/seigenjikan/Clear.kt
@@ -0,0 +1,24 @@
+package com.example.seigenjikan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seigenjikan.databinding.ActivityClearBinding
+
+class Clear : AppCompatActivity() {
+    private lateinit var binding: ActivityClearBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityClearBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_clear)
+
+        binding.TitleButton1.setOnClickListener{ onTitleButtonTapped() }
+
+    }
+
+    fun onTitleButtonTapped() {
+        val intent = Intent(this, GameOver::class.java)
+        startActivity(intent)
+    }
+}
