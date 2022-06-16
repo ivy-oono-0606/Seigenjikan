fcce6f6fc2b307cc8264c95ed93e9d92b8730318
ono_21-10 <ryu.11.10.21.syn.15.4.14@gmail.com>
2022-06-16 11:42:04 +0900

    NPC画面ボタン処理を追加

--- app/src/main/java/com/example/seigenjikan/NPCFragment.kt
+++ app/src/main/java/com/example/seigenjikan/NPCFragment.kt
@@ -1,57 +1,85 @@
 package com.example.seigenjikan
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.seigenjikan.databinding.ActivityMainBinding
+import com.example.seigenjikan.databinding.ActivitySubBinding
+import com.example.seigenjikan.databinding.FragmentNPCBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [NPCFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class NPCFragment : Fragment() {
+    private var _binding: FragmentNPCBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var bindingA: NPCFragment
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    private var param1: String? = null
+//    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        //bindingA = NPCFragment()
+
+        binding.NextImageButton.setOnClickListener{
+            val intent = Intent(activity, SubActivity::class.java)
+            startActivity(intent)
         }
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
     }
+
+
+//    private fun setContentView(root: ConstraintLayout) {
+//
+//    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_n_p_c, container, false)
+        _binding = FragmentNPCBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NPCFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                NPCFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment NPCFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//                NPCFragment().apply {
+//                    arguments = Bundle().apply {
+//                        putString(ARG_PARAM1, param1)
+//                        putString(ARG_PARAM2, param2)
+//                    }
+//                }
+//    }
 }
