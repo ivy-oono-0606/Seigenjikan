bc914fd3f828a5ec1ecb16428a4c17baf49b797b
ユーザー名 <sasanana2479@gmail.com>
2022-06-16 10:29:03 +0900

    タイマーフラグメント廃止

--- app/src/main/java/com/example/seigenjikan/SubActivity.kt
+++ app/src/main/java/com/example/seigenjikan/SubActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.example.seigenjikan.databinding.ActivitySubBinding
-import com.example.seigenjikan.databinding.FragmentTimerBinding
 
 class SubActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySubBinding
-    private lateinit var binding2: FragmentTimerBinding
-    private lateinit var timer: TimerFragment
+    //private lateinit var timer: TimerFragment
     private lateinit var batle: BattleFragment
 
+    //ここからタイマー定義
     inner class MyCountDownTimer(
             millisInFuture: Long,
             countDownInterval: Long
@@ -21,39 +20,59 @@ class SubActivity : AppCompatActivity() {
         override fun onTick(millisUntilFinished: Long) {
             val minute = millisUntilFinished / 1000L / 60L
             val second = millisUntilFinished / 1000L % 60L
-            binding2.timerText.text = "%1d:%2$02d".format(minute, second)
+            binding.timerText.text = "%1d:%2$02d".format(minute, second)
         }
 
         override fun onFinish() {
-            binding2.timerText.text = "0:00"
+            binding.timerText.text = "0:00"
         }
     }
+    //ここまでタイマー定義
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySubBinding.inflate(layoutInflater)
-        binding2 = FragmentTimerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var sikai = IntArray(3)
+        sikai += 20
+        //var sikaillen = sikai.size
+
+
         //ここからタイマーフラグメント、バトルフラグメント表示
-        timer = TimerFragment()
+        //timer = TimerFragment()
         batle = BattleFragment()
         supportFragmentManager.beginTransaction().apply{
-            replace(R.id.TimerFrame, timer)
-            replace(R.id.BattleFrame,batle)
+            //replace(R.id.TimerFrame, timer)
+            replace(R.id.BattleFrame, batle)
             commit()
         }
         //ここまでタイマーフラグメント、バトルフラグメント表示
 
-        //ここからタイマー処理
-        binding2.timerText.text = "3:00"
+        //ここからタイマー操作
+        binding.timerText.text = "3:00"
         var timer = MyCountDownTimer(3 * 60 * 1000, 100)
-        timer.start()
-        timer.isRunning = true
-        //ここまでタイマー処理
+        binding.testbutton.setOnClickListener {
+            timer.isRunning = when (timer.isRunning) {
+                true -> {
+                    //タイマーが動いているときに停止
+                    timer.cancel()
+                    false
+                }
+                false -> {
+                    //タイマーが動いてないときに開始
+                    timer.start()
+                    true
+                }
+            }
+        }
+        //ここまでタイマー操作
 
         //ここからボタン判定
-        fun hanntei(hand:Int){
+        for(i in sikai){
+            binding.timerText.text = sikai[3].toString()
+        }
+        fun hanntei(hand: Int){
             var test : Int = 0
             if (test == hand) {
                 //正解
@@ -64,9 +83,9 @@ class SubActivity : AppCompatActivity() {
         //ここまでボタン判定
 
         //赤ボタン
-        /*binding.RedButton.setOnClickListener{
+        binding.RedButton.setOnClickListener{
             hanntei(0)
-        }*/
+        }
         //赤ボタンここまで
 
         //青ボタン
--- app/src/main/java/com/example/seigenjikan/TimerFragment.kt
+++ app/src/main/java/com/example/seigenjikan/TimerFragment.kt
@@ -1,6 +1,7 @@
 package com.example.seigenjikan
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,36 +9,24 @@ import android.view.ViewGroup
 import com.example.seigenjikan.databinding.ActivitySubBinding
 import com.example.seigenjikan.databinding.FragmentTimerBinding
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 class TimerFragment : Fragment() {
-    lateinit var binding: FragmentTimerBinding
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentTimerBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        /*var timer = MyCountDownTimer(3 * 60 * 1000, 100)
+        timer.start()
+        timer.isRunning = true*/
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                TimerFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        _binding =  FragmentTimerBinding.inflate(inflater, container, false)
+        binding.timerText2.text = "3:00"
+        return binding.root
     }
 }
