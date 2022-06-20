b6caceed3ad5347e3861fd988709eb1e0be3dd7b
ono_21-10 <ryu.11.10.21.syn.15.4.14@gmail.com>
2022-06-20 13:48:44 +0900

    NPCサブアクティビティ追加事項あり

--- app/src/main/java/com/example/seigenjikan/NpcSubActivity.kt
+++ app/src/main/java/com/example/seigenjikan/NpcSubActivity.kt
@@ -2,10 +2,24 @@ package com.example.seigenjikan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seigenjikan.databinding.ActivitySubBinding
 
 class NpcSubActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySubBinding
+    private lateinit var NPC: NPCFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_npc_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NPC = NPCFragment()
+
+        supportFragmentManager.beginTransaction().apply{
+            //replace(R.id.TimerFrame, timer)
+            replace(R.id.HaikeiFrame, NPC)
+            commit()
+        }
     }
 }
