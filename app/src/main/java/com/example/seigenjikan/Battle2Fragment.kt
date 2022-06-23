d303cc87e7c6b345bb990eb9a4c6115bbe2acb42
ono_21-10 <ryu.11.10.21.syn.15.4.14@gmail.com>
2022-06-23 09:39:57 +0900

    バトル2画面追加

---
+++ app/src/main/java/com/example/seigenjikan/Battle2Fragment.kt
@@ -0,0 +1,42 @@
+package com.example.seigenjikan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.seigenjikan.databinding.FragmentBattle2Binding
+import com.example.seigenjikan.databinding.FragmentBattleBinding
+
+
+class Battle2Fragment : Fragment() {
+    private var _binding: FragmentBattle2Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBattle2Binding.inflate(inflater, container, false)
+
+        var position:String? = ""
+        // Bundleを取得する
+        val bundle = arguments
+        // Bundleがセットされていたら値を受け取る
+        if (bundle != null) {
+            position = bundle.getString("KEY_POSITION")
+        }
+        //packageName取得
+        val packageName = BuildConfig.APPLICATION_ID
+        val imageId = resources.getIdentifier(position, "drawable", packageName) //リソースIDのを取得
+
+        binding.Enemy2ImageView.setImageResource(imageId)
+
+        return binding.root
+    }
+}
