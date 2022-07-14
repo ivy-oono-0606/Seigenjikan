1ee97393ff9eaff1c5d118135f37c01a4e854a72
ono_21-10 <ryu.11.10.21.syn.15.4.14@gmail.com>
2022-07-14 15:49:47 +0900

    セレクト画面の機能追加

--- app/src/main/java/com/example/seigenjikan/SelectSubActivity.kt
+++ app/src/main/java/com/example/seigenjikan/SelectSubActivity.kt
@@ -1,11 +1,44 @@
 package com.example.seigenjikan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.seigenjikan.databinding.ActivitySelectSubBinding
 
 class SelectSubActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelectSubBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_sub)
+        binding = ActivitySelectSubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.MsButton.setOnClickListener{onstartbuttontapped(it)}
+        binding.DjButton.setOnClickListener{onDjButtontapped(it)}
+        binding.DragonButton.setOnClickListener{onDragonButtontapped(it)}
+        binding.DevilButton.setOnClickListener{onDevilButtontapped(it)}
     }
+
+    fun onstartbuttontapped(View: View?){
+        val intent = Intent(getApplication(), SubActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onDjButtontapped(View: View?){
+        val intent = Intent(getApplication(), SubActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onDragonButtontapped(View: View?){
+        val intent = Intent(getApplication(), SubActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onDevilButtontapped(View: View?){
+        val intent = Intent(getApplication(), SubActivity::class.java)
+        startActivity(intent)
+    }
+
 }
